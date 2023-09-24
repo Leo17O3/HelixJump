9be8e17786df06d0adac28c753cccb92597db097
Leo17O3 <leonidgorelov09@gmail.com>
2023-09-24 10:26:39 +0300

    Added spawning os Spawn and Finish platforms

--- Assets/Scripts/TowerSpawner.cs
+++ Assets/Scripts/TowerSpawner.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class TowerSpawner : MonoBehaviour
 {
     [SerializeField] private int _levelCount;
+    [SerializeField] private float _additiveScale;
     [SerializeField] private GameObject _beam;
     [SerializeField] private SpawnPlatform _spawnPlatform;
     [SerializeField] private Platform[] _platforms;
@@ -19,17 +20,31 @@ public class TowerSpawner : MonoBehaviour
         beam.transform.localScale = new Vector3(1f, _levelCount / 2, 1f);
 
         Vector3 spawnPosition = beam.transform.position;
-        spawnPosition.y = beam.transform.localScale.y;
+        spawnPosition.y += beam.transform.localScale.y;
 
         for (int i = 0; i < _levelCount; i++)
         {
             SpawnPlatform(_platforms[Random.Range(0, _platforms.Length)], ref spawnPosition, beam.transform);
         }
+
+        beam.transform.localScale = new Vector3(1f, beam.transform.localScale.y + _additiveScale, 1f);
+        spawnPosition.y = beam.transform.localScale.y;
+
+        SpawnPlatform(_spawnPlatform.gameObject, ref spawnPosition, beam.transform);
+        SpawnPlatform(_finishPlatform.gameObject, ref spawnPosition, beam.transform);
+
+        beam.transform.localScale = new Vector3(1f, beam.transform.localScale.y + _additiveScale, 1f);
     }
 
     private void SpawnPlatform(Platform platform, ref Vector3 spawnPosition, Transform parent)
     {
         Instantiate(platform, spawnPosition, Quaternion.Euler(0, Random.Range(0, 360), 0), parent);
         spawnPosition.y -= 1;
+    }
+
+    private void SpawnPlatform(GameObject platformToSpawn, ref Vector3 spawnPosition, Transform parent)
+    {
+        Instantiate(platformToSpawn, spawnPosition, Quaternion.identity, parent);
+        spawnPosition.y *= -1;
     }
 }
