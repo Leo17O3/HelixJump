e96e3c8de0b03ad450ebf25e27a37574f916116b
Leonid <leonidgorelov09@gmail.com>
2023-09-30 19:59:33 +0300

    Addded Player, edited Spawner

--- Assets/Scripts/Platforms/BasePlatform.cs
+++ Assets/Scripts/Platforms/BasePlatform.cs
@@ -2,4 +2,13 @@ using UnityEngine;
 
 public abstract class BasePlatform : MonoBehaviour
 {
+    [SerializeField] private float _jumpForce = 5f;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.TryGetComponent(out Rigidbody rigidbody))
+        {
+            rigidbody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+        }
+    }
 }
--- Assets/Scripts/TowerSpawner.cs
+++ Assets/Scripts/TowerSpawner.cs
@@ -3,11 +3,14 @@ using UnityEngine;
 public class TowerSpawner : MonoBehaviour
 {
     [SerializeField] private int _levelCount;
-    [SerializeField] private float _additiveScale;
+    [SerializeField] private float _additionalScale;
     [SerializeField] private GameObject _beam;
     [SerializeField] private SpawnPlatform _spawnPlatform;
     [SerializeField] private Platform[] _platforms;
     [SerializeField] private FinishPlatform _finishPlatform;
+    private float _startAndFinishAdditionalScale = 0.5f;
+
+    private float _beamScaleY => _additionalScale / 2f + _startAndFinishAdditionalScale + _levelCount / 2f;
 
     private void Start()
     {
@@ -17,10 +20,10 @@ public class TowerSpawner : MonoBehaviour
     private void Build()
     {
         GameObject beam = Instantiate(_beam, transform);
-        beam.transform.localScale = new Vector3(1f, _levelCount / 2, 1f);
+        beam.transform.localScale = new Vector3(1f, _beamScaleY, 1f);
 
         Vector3 spawnPosition = beam.transform.position;
-        spawnPosition.y += beam.transform.localScale.y + _additiveScale;
+        spawnPosition.y += beam.transform.localScale.y - _additionalScale;
 
         SpawnPlatform(_spawnPlatform, ref spawnPosition, Quaternion.identity, beam.transform);
 
@@ -29,28 +32,12 @@ public class TowerSpawner : MonoBehaviour
             SpawnPlatform(_platforms[Random.Range(0, _platforms.Length)], ref spawnPosition, Quaternion.Euler(0, Random.Range(0, 360), 0), beam.transform);
         }
 
-        beam.transform.localScale = new Vector3(1f, beam.transform.localScale.y + _additiveScale, 1f);
-
-        Vector3 beamScale = new Vector3(0f, -beam.transform.localScale.y, 0f);
-
-        SpawnPlatform(_finishPlatform, ref beamScale, Quaternion.identity, beam.transform);
-
-        //beam.transform.localScale = new Vector3(1f, beam.transform.localScale.y + _additiveScale, 1f);
-        SpawnAdditiveGameObject(beam.transform.position.y + beam.transform.localScale.y + _additiveScale, beam.transform);
-        SpawnAdditiveGameObject(-beam.transform.position.y - beam.transform.localScale.y - _additiveScale, beam.transform);
+        SpawnPlatform(_finishPlatform, ref spawnPosition, Quaternion.identity, beam.transform);
     }
 
     private void SpawnPlatform(BasePlatform platform, ref Vector3 spawnPosition, Quaternion rotation, Transform parent)
     {
         Instantiate(platform, spawnPosition, rotation, parent);
         spawnPosition.y -= 1;
-    }
-
-    private void SpawnAdditiveGameObject(float positionY, Transform parent)
-    {
-        Transform additiveTransform = GameObject.CreatePrimitive(PrimitiveType.Cylinder).transform;
-        additiveTransform.localScale = new Vector3(1f, _additiveScale, 1f);
-        additiveTransform.position = new Vector3(0f, positionY, 0f);
-        additiveTransform.parent = parent;
     }
 }
